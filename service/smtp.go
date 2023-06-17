37c430429fd1f8dbf9941b113d11ca773aa78def
Guga Figueiredo <gugabfigueiredo@gmail.com>
2023-06-17 16:37:56 -0300

    added smtp service and utils

---
+++ service/smtp.go
@@ -0,0 +1,48 @@
+package service
+
+import (
+	"fmt"
+	"github.com/gugabfigueiredo/dream-mail-go/log"
+	"github.com/gugabfigueiredo/dream-mail-go/models"
+	"net/smtp"
+)
+
+type SMTPConfig struct {
+	Host string `json:"host"`
+	Port string `json:"port"`
+	User string `json:"user"`
+	Pass string `json:"pass"`
+}
+
+type SMTPProvider struct {
+	Addr   string
+	Auth   smtp.Auth
+	Logger *log.Logger
+}
+
+func newSMTPProvider(cfg SMTPConfig, logger *log.Logger) *SMTPProvider {
+	return &SMTPProvider{
+		Addr:   fmt.Sprintf("%s:%s", cfg.Host, cfg.Port),
+		Auth:   smtp.PlainAuth("", cfg.User, cfg.Pass, cfg.Host),
+		Logger: logger.C("provider", "smtp"),
+	}
+}
+
+func (s *SMTPProvider) SendMail(mail *models.Mail) error {
+
+	var tos []string
+	for _, to := range mail.To {
+		tos = append(tos, to.Addr)
+	}
+
+	msg := buildSMTPMessage(mail, tos)
+
+	err := smtp.SendMail(s.Addr, s.Auth, mail.From.Addr, tos, msg)
+	if err != nil {
+		s.Logger.E("unable to send email", "err", err)
+		return err
+	}
+
+	s.Logger.I("email sent", "from", mail.From.Addr, "to", tos)
+	return nil
+}
---
+++ service/utils.go
@@ -0,0 +1,43 @@
+package service
+
+import (
+	"fmt"
+	"github.com/gugabfigueiredo/dream-mail-go/models"
+	"path/filepath"
+	"strings"
+)
+
+func buildSMTPMessage(mail *models.Mail, tos []string) []byte {
+	//build message from mail
+	msg := fmt.Sprintf("From: %s\r\n", mail.From.Addr)
+	msg += fmt.Sprintf("To: %s\r\n", strings.Join(tos, ","))
+	msg += fmt.Sprintf("Subject: %s\r\n", mail.Subject)
+
+	msg += "MIME-Version: 1.0\r\n"
+	msg += fmt.Sprintf("Content-Type: multipart/mixed; boundary=\"dmailboundary\"\r\n\r\n")
+
+	if mail.Text != "" {
+		msg += "\r\n--dmailboundary\r\n"
+		msg += "Content-Type: text/plain; charset=\"utf-8\"\r\n"
+		msg += fmt.Sprintf("\r\n%s\r\n\r\n", mail.Text)
+	}
+
+	if mail.Html != "" {
+		msg += "\r\n--dmailboundary\r\n"
+		msg += "Content-Type: text/html; charset=\"utf-8\"\r\n"
+		msg += fmt.Sprintf("\r\n%s\r\n\r\n", mail.Html)
+	}
+
+	for _, attachment := range mail.Attachments {
+		_, fileName := filepath.Split(attachment.Name)
+
+		msg += "\r\n--dmailboundary--\r\n"
+		msg += fmt.Sprintf("Content-Type: %s; charset=\"utf-8\"\r\n", attachment.Type)
+		msg += fmt.Sprintf("Content-Disposition: attachment;filename=\"%s\"\r\n", fileName)
+		msg += fmt.Sprintf("\r\n%s\r\n\r\n", attachment.Data)
+	}
+
+	msg += "\r\n--dmailboundary--\r\n"
+
+	return []byte(msg)
+}
