07d9c0f6c8732d43a26a3fcfd5dd344237140ec7
Guga Figueiredo <gugabfigueiredo@gmail.com>
2023-06-17 16:38:49 -0300

    update mail handler

--- handler/mail.go
+++ handler/mail.go
@@ -1,16 +1,70 @@
 package handler
 
 import (
+	"encoding/json"
+	"github.com/google/uuid"
 	"github.com/gugabfigueiredo/dream-mail-go/log"
+	"github.com/gugabfigueiredo/dream-mail-go/models"
 	"github.com/gugabfigueiredo/dream-mail-go/service"
 	"net/http"
 )
 
+type Response struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
 type Handler struct {
-	Logger    *log.Logger
-	Providers []service.IProvider
+	Service      service.IService
+	Logger       *log.Logger
+	MailingQueue chan models.Mail
+	RetryQueue   chan models.Mail
+}
+
+func NewHandler(service service.IService, logger *log.Logger) *Handler {
+	return &Handler{
+		Service: service,
+		Logger:  logger,
+	}
 }
 
 func (h *Handler) HandleSend(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Email Sent\n"))
+
+	logger := h.Logger.C()
+
+	mail, err := readMailFromRequest(r)
+	if err != nil {
+		logger.E("invalid or corrupted e-mail data", "err", err)
+		http.Error(w, "invalid or corrupted e-mail data", http.StatusBadRequest)
+	}
+	// queue mail for delivery
+	h.Service.QueueMail(mail)
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode([]byte(`{"status":  "OK", "message": "e-mail queued for delivery"}`)); err != nil {
+		logger.E("error on json encoding", "err", err)
+		http.Error(w, "error writing response", http.StatusInternalServerError)
+		return
+	}
+
+	logger.I("e-mail queued for delivery")
+}
+
+// readMailFromRequest gets the email to be sent with all specs and unique ID, it returns error if mail is missing info
+func readMailFromRequest(r *http.Request) (*models.Mail, error) {
+
+	var mail models.Mail
+	if err := json.NewDecoder(r.Body).Decode(&mail); err != nil {
+		return &models.Mail{}, err
+	}
+
+	if ok, err := mail.Validate(); !ok {
+		return &models.Mail{}, err
+	}
+
+	if mail.ID == "" {
+		mail.ID = uuid.New().String()
+	}
+
+	return &mail, nil
 }
