085d533f19078cd4223dba2b32eff7ca823cc935
Guga Figueiredo <gugabfigueiredo@gmail.com>
2023-06-17 16:41:21 -0300

    main server

--- cmd/dream-mail-server/main.go
+++ cmd/dream-mail-server/main.go
@@ -1,21 +1,63 @@
 package main
 
 import (
-	"github.com/gugabfigueiredo/dream-mail-go/handler"
-	"net/http"
-
+	"fmt"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/gugabfigueiredo/dream-mail-go/env"
+	"github.com/gugabfigueiredo/dream-mail-go/handler"
+	"github.com/gugabfigueiredo/dream-mail-go/log"
+	"github.com/gugabfigueiredo/dream-mail-go/service"
+	"github.com/kelseyhightower/envconfig"
+	"net/http"
+	"os"
+	"time"
 )
+
+var Logger *log.Logger
+
+func init() {
+	envconfig.MustProcess("dmail", &env.Settings)
+
+	Logger = log.New(env.Settings.Log)
+
+	name, _ := os.Hostname()
+	Logger = Logger.C("host", name)
+
+}
 
 func main() {
 
-	mailHandler := handler.Handler{}
+	// Start service
+	mailService := service.NewService(env.Settings.Service, Logger)
 
+	// Handlers
+	mailHandler := handler.NewHandler(mailService, Logger)
+
+	// Start server
 	r := chi.NewRouter()
-
-	r.Route("/mail", func(r chi.Router) {
+	r.Use(middleware.Logger)
+	r.Get("/ping", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = w.Write([]byte("pong"))
+	})
+	r.Route(fmt.Sprintf("/%s", env.Settings.Server.Context), func(r chi.Router) {
 		r.Post("/send", mailHandler.HandleSend)
 	})
 
-	http.ListenAndServe(":3000", r)
+	http.Handle("/", r)
+
+	server := &http.Server{
+		Addr:           fmt.Sprintf(":%s", env.Settings.Server.Port),
+		Handler:        nil,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	Logger.I("Starting server...", "port", env.Settings.Server.Port)
+
+	if err := server.ListenAndServe(); err != nil {
+		mailService.Quit()
+		Logger.F("listen and serve died", "err", err)
+	}
 }
--- go.mod
+++ go.mod
@@ -3,13 +3,28 @@ module github.com/gugabfigueiredo/dream-mail-go
 go 1.20
 
 require (
+	github.com/SparkPost/gosparkpost v0.2.0
+	github.com/aws/aws-sdk-go v1.44.284
 	github.com/go-chi/chi/v5 v5.0.8
+	github.com/google/uuid v1.3.0
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/mailgun/mailgun-go/v4 v4.9.0
+	github.com/pkg/errors v0.9.1
 	github.com/rs/zerolog v1.29.1
+	github.com/sendgrid/rest v2.6.9+incompatible
+	github.com/sendgrid/sendgrid-go v3.12.0+incompatible
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1
 )
 
 require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
-	golang.org/x/sys v0.0.0-20210927094055-39ccf1dd6fa6 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 )
