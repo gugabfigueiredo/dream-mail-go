18649ff44ba98ebb2a24dfed0c303408d1d95898
Guga Figueiredo <gugabfigueiredo@gmail.com>
2023-06-17 20:36:02 -0300

    adjustments to smtp message strings

--- service/utils.go
+++ service/utils.go
@@ -14,30 +14,30 @@ func buildSMTPMessage(mail *models.Mail, tos []string) []byte {
 	msg += fmt.Sprintf("Subject: %s\r\n", mail.Subject)
 
 	msg += "MIME-Version: 1.0\r\n"
-	msg += fmt.Sprintf("Content-Type: multipart/mixed; boundary=\"dmailboundary\"\r\n\r\n")
+	msg += fmt.Sprintf("Content-Type: multipart/mixed; boundary=\"dmailboundary\"\r\n")
 
 	if mail.Text != "" {
 		msg += "\r\n--dmailboundary\r\n"
 		msg += "Content-Type: text/plain; charset=\"utf-8\"\r\n"
-		msg += fmt.Sprintf("\r\n%s\r\n\r\n", mail.Text)
+		msg += fmt.Sprintf("\r\n%s\r\n", mail.Text)
 	}
 
-	if mail.Html != "" {
+	if mail.HTML != "" {
 		msg += "\r\n--dmailboundary\r\n"
 		msg += "Content-Type: text/html; charset=\"utf-8\"\r\n"
-		msg += fmt.Sprintf("\r\n%s\r\n\r\n", mail.Html)
+		msg += fmt.Sprintf("\r\n%s\r\n", mail.HTML)
 	}
 
 	for _, attachment := range mail.Attachments {
 		_, fileName := filepath.Split(attachment.Name)
 
-		msg += "\r\n--dmailboundary--\r\n"
+		msg += "\r\n--dmailboundary\r\n"
 		msg += fmt.Sprintf("Content-Type: %s; charset=\"utf-8\"\r\n", attachment.Type)
 		msg += fmt.Sprintf("Content-Disposition: attachment;filename=\"%s\"\r\n", fileName)
-		msg += fmt.Sprintf("\r\n%s\r\n\r\n", attachment.Data)
+		msg += fmt.Sprintf("\r\n%s\r\n", attachment.Data)
 	}
 
-	msg += "\r\n--dmailboundary--\r\n"
+	msg += "\r\n--dmailboundary\r\n"
 
 	return []byte(msg)
 }
